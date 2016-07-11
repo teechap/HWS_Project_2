222b6ba3023a692d94cc40ef6970e18e63b19716
Travis <travisechapman@gmail.com>
2016-07-11 12:25:36 -0400

    Initial Commit

---
+++ HWS_Project_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HWS_Project_2
+//
+//  Created by Travis Chapman on 7/11/16.
+//  Copyright © 2016 Travis Chapman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
